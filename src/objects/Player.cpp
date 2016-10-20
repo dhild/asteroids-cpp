1e48d203d9cfd5b3e5def8a1a145b54296486974
D. Ryan Hild <d.ryan.hild@gmail.com>
2016-10-19 19:22:33 -0700

    Fix velocity at maximum

--- src/objects/Player.cpp
+++ src/objects/Player.cpp
@@ -1,9 +1,11 @@
 #include <cmath>
+#include <Poco/Logger.h>
 #include "Player.hpp"
 
 using namespace objects;
 
 static const float pi = std::acos(-1.0f);
+static const float MAX_VELOCITY_SQUARED = 0.003f;
 
 Player::Player() : position(0, 0), orientation(0), velocity(0, 0) {}
 
@@ -17,8 +19,10 @@ void Player::tick(bool accelerate, bool rotateLeft, bool rotateRight) {
     acc *= 0.0005;
     velocity += acc;
     float mag2 = (velocity.x * velocity.x) + (velocity.y * velocity.y);
-    if (mag2 > 0.25f) {
-      velocity *= (mag2 / 0.25f);
+    if (mag2 > MAX_VELOCITY_SQUARED) {
+      velocity *= MAX_VELOCITY_SQUARED / mag2;
+      static Poco::Logger& logger = Poco::Logger::get("Player");
+      logger.debug("Exceeded maximum velocity");
     }
   } else {
     velocity *= 0.99f;
